9332ac669d037140d11da85d7c7b92bc42c6e384
jacob_lites <jnightlight@gmail.com>
2024-05-10 14:48:17 -0700

    Continued splitting out input vs. processing responsibilities.  This will really help the mental while keeping track of what elements are changing based on what inputs. Also makes it a lot easier to make a key-map for actions in case I do a Insert/Normal mode split like VIM

--- main.py
+++ main.py
@@ -30,24 +30,20 @@ class InputActions(Enum):
     RESIZE = 4
 
 
-def process_input(in_char, cur_string, size_properties, window_manager, arrow_key_index, flat_list):
+def process_input(in_char, size_properties, window_manager, arrow_key_index, flat_list):
     input_action = InputActions.BACKSPACE
-    # Replace current search string
-    flat_list_element = []
+
     # Backspace back a character
     if in_char == curses.ascii.BS or in_char == curses.ascii.DEL or in_char == curses.KEY_BACKSPACE or in_char == curses.KEY_DC:
         input_action = InputActions.BACKSPACE
-        # If we're at the beginning, noop. Otherwise, pop most recent char and queue up an updated search
-        if len(cur_string) > 0:
-            cur_string = cur_string[:-1]
+
     # Screen resize
     elif in_char == curses.KEY_RESIZE:
         input_action = InputActions.RESIZE
-        TerminalSizeProperties.resize_screens(size_properties, window_manager)
-        # Clearing here in case there's not an out of bounds draw on a future refresh accidentally
-        window_manager.clear_all_windows()
+
     # Incoming arrow keys for navigating key window
     elif in_char in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_RIGHT]:
+        input_action = InputActions.CHANGE_INDEX
         if in_char == curses.KEY_UP:
             input_action = InputActions.CHANGE_INDEX
             arrow_key_index -= 1
@@ -56,17 +52,12 @@ def process_input(in_char, cur_string, size_properties, window_manager, arrow_ke
                 arrow_key_index = len(flat_list)
         elif in_char == curses.KEY_DOWN:
             arrow_key_index += 1
-        flat_list_element = flat_list[arrow_key_index % len(flat_list)]
-        cur_string = flat_list_element[-1]
-        cur_string = cur_string.strip('\n')
     elif in_char == curses.KEY_LEFT:
         input_action = InputActions.CLEAR_SEARCH
-        cur_string = ''
+    # Any other ascii char //TODO: enter key breaks this, lol. Good for fast exit
     elif curses.ascii.isascii(in_char):
         input_action = InputActions.NEW_CHAR
-        window_manager.key_window.addch(size_properties.key_window_rows - 1, len(cur_string), in_char)
-        cur_string += chr(in_char)
-    return cur_string, arrow_key_index, flat_list_element, input_action
+    return arrow_key_index, input_action
 
 
 def main(stdscr):
@@ -102,12 +93,34 @@ def main(stdscr):
             continue
 
         # This is where we process the incoming character and update some data
-        cur_string, arrow_key_index, flat_list_element, input_action = process_input(in_char,
-                                                                                      cur_string,
-                                                                                      size_properties,
-                                                                                      window_manager,
-                                                                                      arrow_key_index,
-                                                                                      flat_list)
+        arrow_key_index, input_action = process_input(in_char,
+                                                      size_properties,
+                                                      window_manager,
+                                                      arrow_key_index,
+                                                      flat_list)
+
+        flat_list_element = []
+        match input_action:
+            case InputActions.BACKSPACE:
+                # If we're at the beginning, noop. Otherwise, pop most recent char and queue up an updated search
+                if len(cur_string) > 0:
+                    cur_string = cur_string[:-1]
+                pass
+            case InputActions.NEW_CHAR:
+                window_manager.key_window.addch(size_properties.key_window_rows - 1, len(cur_string), in_char)
+                cur_string += chr(in_char)
+                pass
+            case InputActions.CHANGE_INDEX:
+                flat_list_element = flat_list[arrow_key_index % len(flat_list)]
+                cur_string = flat_list_element[-1]
+                cur_string = cur_string.strip('\n')
+            case InputActions.CLEAR_SEARCH:
+                cur_string = ''
+                pass
+            case InputActions.RESIZE:
+                TerminalSizeProperties.resize_screens(size_properties, window_manager)
+                # Clearing here in case there's not an out of bounds draw on a future refresh accidentally
+                window_manager.clear_all_windows()
 
         #Adding the current text string to the key window. This can probably be moved to KeyWindow soon
         window_manager.key_window.addnstr(size_properties.key_window_rows - 1, 0, cur_string,
