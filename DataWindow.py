9b7496044ae577de07ac3ab260ea9c618c31e3da
jacob_lites <jnightlight@gmail.com>
2024-05-10 16:17:07 -0700

    Setup initial word formatting system. Printing word by word, will possibly cache the answers later to not rebuild this whole thing from scratch every time

--- DataWindow.py
+++ DataWindow.py
@@ -23,7 +23,6 @@ def print_data_window_data(window_manager, app_data_dictionary, size_properties,
 
 
 def print_data_window_string(window_manager, size_properties, to_print):
-    line_list_index = 0
     line_list = []
     cur_line = INDENT_STRING
     word_list = to_print.split(" ")
@@ -33,19 +32,74 @@ def print_data_window_string(window_manager, size_properties, to_print):
         if len(cur_line) + len(word) >= size_properties.data_window_cols:
             line_list.append(cur_line)
             cur_line = word + " "
-            line_list_index += 1
         else:
             cur_line += word + " "
         if "\n" in word:
             line_break_split_list = word.split("\n")
             line_list.append(cur_line)
             cur_line = INDENT_STRING + line_break_split_list[1] + " "
-            line_list_index += 1
 
     print_index = 0
     while print_index < len(line_list) and print_index < size_properties.data_window_rows:
         window_manager.data_window.addnstr(print_index, 0, line_list[print_index], size_properties.data_window_cols)
         print_index += 1
+
+
+class WordAndFormat:
+    word = ''
+    curses_attributes = curses.A_NORMAL
+
+    def __init__(self, word, curses_attributes):
+        self.word = word
+        self.format = curses_attributes
+
+
+def print_data_window_string_by_word(window_manager, size_properties, to_print):
+    current_visual_line = INDENT_STRING
+    line_list = []
+    line_words = []
+    word_list = to_print.split(" ")
+    formatting = curses.A_NORMAL
+    for word in word_list:
+        if len(word) <= 0:
+            continue
+
+        if word == "<bold>":
+            formatting = formatting | curses.A_BOLD | curses.A_ITALIC | curses.A_BLINK
+            continue
+        if word == "</bold>":
+            formatting = curses.A_NORMAL
+            continue
+
+        fancyWord = WordAndFormat(word, formatting)
+        if len(current_visual_line) + len(word) >= size_properties.data_window_cols:
+            line_list.append(line_words)
+            current_visual_line = word + " "
+            line_words = [fancyWord]
+        else:
+            current_visual_line += word + " "
+            line_words.append(fancyWord)
+
+        if "\n" in word:
+            line_break_split_list = word.split("\n")
+            line_words[-1].word = line_break_split_list[0]
+            line_list.append(line_words)
+            current_visual_line = INDENT_STRING + line_break_split_list[1] + " "
+            fancyWord = WordAndFormat(INDENT_STRING + line_break_split_list[1], formatting)
+            line_words = [fancyWord]
+
+    word_index = 0
+    line_index = 0
+    cur_x = 0
+    while line_index < len(line_list) and line_index < size_properties.data_window_rows:
+        for fancyWord in line_list[line_index]:
+            window_manager.data_window.addnstr(line_index, cur_x, fancyWord.word, size_properties.data_window_cols,
+                                               fancyWord.format)
+            word_index += 1
+            cur_x += len(fancyWord.word) + 1
+        cur_x = 0
+        word_index = 0
+        line_index += 1
 
 
 def print_data_window_text(found_element, window_manager, size_properties):
@@ -54,6 +108,6 @@ def print_data_window_text(found_element, window_manager, size_properties):
         for element in found_element:
             if isinstance(element, dict):
                 to_print += str(element.keys()) + "\n"
-                print_data_window_string(window_manager, size_properties, to_print)
+                print_data_window_string_by_word(window_manager, size_properties, to_print)
     elif isinstance(found_element, str):
-        print_data_window_string(window_manager, size_properties, found_element)
+        print_data_window_string_by_word(window_manager, size_properties, found_element)
