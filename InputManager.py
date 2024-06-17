3d69c9facc1e640a52e6884b7582d10fd4566a46
jacob_lites <jnightlight@gmail.com>
2024-06-17 10:01:04 -0700

    Adding "InputManager" to keep key reading work encapsulated

---
+++ InputManager.py
@@ -0,0 +1,40 @@
+import curses.ascii
+from enum import Enum
+
+
+class InputActions(Enum):
+    BACKSPACE = 0
+    NEW_CHAR = 1
+    CHANGE_INDEX = 2
+    CLEAR_SEARCH = 3
+    RESIZE = 4
+
+
+def process_input(in_char, arrow_key_index, flat_list):
+    input_action = InputActions.BACKSPACE
+
+    # Backspace back a character
+    if in_char == curses.ascii.BS or in_char == curses.ascii.DEL or in_char == curses.KEY_BACKSPACE or in_char == curses.KEY_DC:
+        input_action = InputActions.BACKSPACE
+
+    # Screen resize
+    elif in_char == curses.KEY_RESIZE:
+        input_action = InputActions.RESIZE
+
+    # Incoming arrow keys for navigating key window
+    elif in_char in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_RIGHT]:
+        input_action = InputActions.CHANGE_INDEX
+        if in_char == curses.KEY_UP:
+            input_action = InputActions.CHANGE_INDEX
+            arrow_key_index -= 1
+            # Wraparound
+            if arrow_key_index < 0:
+                arrow_key_index = len(flat_list)
+        elif in_char == curses.KEY_DOWN:
+            arrow_key_index += 1
+    elif in_char == curses.KEY_LEFT:
+        input_action = InputActions.CLEAR_SEARCH
+    # Any other ascii char //TODO: enter key breaks this, lol. Good for fast exit
+    elif curses.ascii.isascii(in_char):
+        input_action = InputActions.NEW_CHAR
+    return arrow_key_index, input_action
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import curses
 import curses.ascii
 import os
 import sys
-from enum import Enum
 
 import DataWindow
+import InputManager
 import KeyWindow
 import TerminalSizeProperties
 import WindowManager
 import helperFunctions
 import loadFile
+from InputManager import InputActions
 
 MAX_VALID_DISPLAY_COLS = 10
 MAX_VALID_DISPLAY_ROWS = 10
@@ -22,58 +23,20 @@ def create_windows(size_properties):
     return window_manager
 
 
-class InputActions(Enum):
-    BACKSPACE = 0
-    NEW_CHAR = 1
-    CHANGE_INDEX = 2
-    CLEAR_SEARCH = 3
-    RESIZE = 4
-
-
-def process_input(in_char, size_properties, window_manager, arrow_key_index, flat_list):
-    input_action = InputActions.BACKSPACE
-
-    # Backspace back a character
-    if in_char == curses.ascii.BS or in_char == curses.ascii.DEL or in_char == curses.KEY_BACKSPACE or in_char == curses.KEY_DC:
-        input_action = InputActions.BACKSPACE
-
-    # Screen resize
-    elif in_char == curses.KEY_RESIZE:
-        input_action = InputActions.RESIZE
-
-    # Incoming arrow keys for navigating key window
-    elif in_char in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_RIGHT]:
-        input_action = InputActions.CHANGE_INDEX
-        if in_char == curses.KEY_UP:
-            input_action = InputActions.CHANGE_INDEX
-            arrow_key_index -= 1
-            # Wraparound
-            if arrow_key_index < 0:
-                arrow_key_index = len(flat_list)
-        elif in_char == curses.KEY_DOWN:
-            arrow_key_index += 1
-    elif in_char == curses.KEY_LEFT:
-        input_action = InputActions.CLEAR_SEARCH
-    # Any other ascii char //TODO: enter key breaks this, lol. Good for fast exit
-    elif curses.ascii.isascii(in_char):
-        input_action = InputActions.NEW_CHAR
-    return arrow_key_index, input_action
-
-
 def main(stdscr):
     # Bail if not a file
     if not os.path.isfile(sys.argv[1]):
         exit(1)
 
-    #Since it's a valid file, try to load it and populate the flat list. For now just bail if we can't parse
+    # Since it's a valid file, try to load it and populate the flat list. For now just bail if we can't parse
     app_data_dictionary = loadFile.populate_app_data_yaml(sys.argv[1])
     flat_list = helperFunctions.get_flat_key_list(app_data_dictionary)
 
-    #Initialize GUI with curses
+    # Initialize GUI with curses
     size_properties = TerminalSizeProperties.TerminalSizeProperties(curses.LINES, curses.COLS)
     window_manager = create_windows(size_properties)
 
-    #Initial printing of data
+    # Initial printing of data
     KeyWindow.print_advanced_keys_recursive(window_manager.key_box_window, app_data_dictionary, [], size_properties, 0,
                                             1)
 
@@ -82,22 +45,18 @@ def main(stdscr):
     arrow_key_index = 0
     loop = True
     while loop:
-        #Start the loop simply: refresh updated screens to user, then wait on chracter input
+        # Start the loop simply: refresh updated screens to user, then wait on chracter input
         window_manager.refresh_and_draw_boxes()
         in_char = window_manager.key_window.getch(size_properties.key_window_rows - 1, cur_char_x)
 
-        #Strictly unnecessary and frankly wasteful, but hey, nothing here is crucial enough for this level of
+        # Strictly unnecessary and frankly wasteful, but hey, nothing here is crucial enough for this level of
         #  work. TODO maybe clean it up and only refresh when necessary when feature complete
         window_manager.clear_all_windows()
         if curses.LINES <= MAX_VALID_DISPLAY_ROWS or curses.COLS <= MAX_VALID_DISPLAY_COLS:
             continue
 
         # Updating the arrow key index (if it's changed) and grabbing the input action to work off of.
-        arrow_key_index, input_action = process_input(in_char,
-                                                      size_properties,
-                                                      window_manager,
-                                                      arrow_key_index,
-                                                      flat_list)
+        arrow_key_index, input_action = InputManager.process_input(in_char, arrow_key_index, flat_list)
 
         flat_list_element = []
         # Processing our state based on incoming key
@@ -132,12 +91,12 @@ def main(stdscr):
         window_manager.key_window.addnstr(size_properties.key_window_rows - 1, 0, cur_string,
                                           size_properties.key_window_cols - 2)
 
-        #Refreshing the keys in the KeyWindow
+        # Refreshing the keys in the KeyWindow
         KeyWindow.print_advanced_keys_recursive(window_manager.key_box_window, app_data_dictionary, matching_searches,
                                                 size_properties,
                                                 0, 1)
 
-        if (len(matching_searches) == 1):
+        if len(matching_searches) == 1:
             found_element = helperFunctions.get_element_from_flat_index(app_data_dictionary, matching_searches[0])
             DataWindow.print_data_window_text(found_element, window_manager, size_properties)
 
